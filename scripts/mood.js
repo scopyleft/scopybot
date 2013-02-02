a71680f8365035da10ac37e693b40538f0cd06c2
Nicolas Perriault <nperriault@gmail.com>
2013-02-02 16:09:21 +0100

    refactored mood fecthing API, introduced filters

--- scripts/mood.js
+++ scripts/mood.js
@@ -15,7 +15,8 @@
 
 /*global module process*/
 var bot
-  , format     = require('util').format
+  , util       = require('util')
+  , format     = util.format
   , validMoods = "sunny|cloudy|rainy|stormy".split('|')
   , bars       = "▇▅▃▂".split('')
   , redisKey   = "moods"
@@ -24,6 +25,13 @@ var bot
 
 
 function datetime(date) {
+    if (typeof date === "string") {
+        try {
+            date = new Date(date);
+        } catch (e) {
+            return date;
+        }
+    }
     return date.toISOString().split('T')[0];
 }
 
@@ -40,10 +48,36 @@ function yesterday() {
     return datetime(daysBefore(1));
 }
 
-function getMoods(cb) {
+function filterMoods(moods, filters) {
+    if (!moods.length || !filters) {
+        return [];
+    }
+    return moods.filter(function(mood) {
+        var included = true;
+        if (filters.date) {
+            included = included && datetime(mood.date) === datetime(filters.date);
+        }
+        if (~~filters.since > 0) {
+            included = included && datetime(mood.date) >= datetime(daysBefore(~~filters.since));
+        }
+        if (filters.user) {
+            included = included && mood.user === filters.user;
+        }
+        return included;
+    });
+}
+
+function getMoods() {
+    var filters = {}, cb;
+    if (arguments.length === 2 && typeof arguments[1] === "function") {
+        filters = typeof arguments[0] === "object" ? arguments[0] : {};
+        cb = arguments[1];
+    } else {
+        cb = arguments[0];
+    }
     this.lrange(redisKey, 0, -1, function(err, reply) {
-        var moods;
-        if (err) cb(err);
+        var moods = [];
+        if (err) return cb(err);
         try {
             moods = reply.map(function(entry) {
                 var parts = entry.split(':');
@@ -56,15 +90,27 @@ function getMoods(cb) {
         } catch (e) {
             err = e;
         }
-        cb(err, moods || []);
+        return cb(err, filterMoods(moods, filters));
     });
 }
 
-function moodExists(date, user, cb) {
-    getMoods.call(this, function(err, moods) {
-        cb(err, moods.some(function(mood) {
-            return mood.date === today() && mood.user === user;
-        }));
+function moodExists(filters, cb) {
+    getMoods.call(this, filters, function(err, moods) {
+        return cb(err, moods.length > 0);
+    });
+}
+
+function moodGraph(filters, cb) {
+    if (!filters.user) return cb(new Error("a user is mandatory"));
+    if (!filters.since) return cb(new Error("a since filter is mandatory"));
+    getMoods.call(this, filters, function(err, moods) {
+        var graph = "";
+        if (!moods || !moods.length) {
+            return cb(new Error(format('No mood entry for %s in the last %d days.', filters.user, filters.since)));
+        }
+        return cb(null, moods.map(function(mood) {
+            return bars[validMoods.indexOf(mood.mood)];
+        }).join(''));
     });
 }
 
@@ -72,12 +118,16 @@ function storeMood(user, mood, cb) {
     var client = this,
         date   = today()
       , entry  = format('%s:%s:%s', date, user, mood);
-    moodExists.call(client, date, user, function(err, exists) {
+    if (validMoods.indexOf(mood) === -1) {
+        return cb(new Error(format('Invalid mood %s; valid values are %s',
+                                   mood, validMoods.join(', '))));
+    }
+    moodExists.call(client, { date: date, user: user }, function(err, exists) {
         if (err) throw err;
         if (exists) {
-            return bot.send(format('mood already stored for %s on %s', user, date));
+            return cb(new Error(format('Mood already stored for %s on %s', user, date)));
         }
-        bot.logger.info('storing mood entry: ' + entry);
+        bot.logger.info(format('storing mood entry for %s: %s', user, entry));
         client.rpush(redisKey, entry, cb);
     });
 }
@@ -103,26 +153,18 @@ module.exports = function(robot) {
     robot.respond(/mood set (\w+)$/i, function(msg) {
         var user = msg.message.user && msg.message.user.name || "anon"
          ,  mood = msg.match[1].toLowerCase();
-        if (validMoods.indexOf(mood) === -1) {
-            return msg.send(format('Invalid mood %s; valid values are %s',
-                                   mood, validMoods.join(', ')));
-        }
         storeMood.call(client, user, mood, function(err, reply) {
-            if (err) throw err;
+            if (err) return msg.send(err);
             msg.send(format('Recorded entry: %s is in a %s mood today', user, mood));
         });
     });
 
     robot.respond(/mood today$/i, function(msg) {
         msg.send(format("Today's moods:"));
-        getMoods.call(client, function(err, moods) {
-            var todayMoods = moods.filter(function(mood) {
-                return mood.date === today();
-            });
-            if (!todayMoods || !todayMoods.length) {
-                return msg.send('No mood entry for today.');
-            }
-            todayMoods.forEach(function(mood) {
+        getMoods.call(client, { date: today() }, function(err, moods) {
+            if (err) return msg.send(err);
+            if (!moods || !moods.length) return msg.send('No mood entry for today.');
+            moods.forEach(function(mood) {
                 msg.send(format('- %s is in a %s mood', mood.user, mood.mood));
             });
         });
@@ -130,16 +172,23 @@ module.exports = function(robot) {
 
     robot.respond(/mood yesterday$/i, function(msg) {
         msg.send(format("Yesterday's moods:"));
-        getMoods.call(client, function(err, moods) {
-            var yesterdayMoods = moods.filter(function(mood) {
-                return mood.date === yesterday();
-            });
-            if (!yesterdayMoods || !yesterdayMoods.length) {
-                return msg.send('No mood entry for yesterday.');
-            }
-            yesterdayMoods.forEach(function(mood) {
+        getMoods.call(client, { date: yesterday() }, function(err, moods) {
+            if (err) return msg.send(err);
+            if (!moods || !moods.length) return msg.send('No mood entry for yesterday.');
+            moods.forEach(function(mood) {
                 msg.send(format('- %s was in a %s mood', mood.user, mood.mood));
             });
+        });
+    });
+
+    robot.respond(/mood month (of|for) (.*)$/i, function(msg) {
+        var user = msg.match[2];
+        if (user.toLowerCase().trim() === "me") {
+            user = msg.message.user && msg.message.user.name;
+        }
+        moodGraph.call(client, { user: user, since: 30 }, function(err, graph) {
+            if (err) return msg.send(err);
+            msg.send(graph);
         });
     });
 
@@ -148,17 +197,9 @@ module.exports = function(robot) {
         if (user.toLowerCase().trim() === "me") {
             user = msg.message.user && msg.message.user.name;
         }
-        getMoods.call(client, function(err, moods) {
-            var graph = ""
-              , weekMoods = moods.filter(function(mood) {
-                return mood.user === user && new Date(mood.date) >= daysBefore(7);
-            });
-            if (!weekMoods || !weekMoods.length) {
-                return msg.send(format('No mood entry for %s in the last 7 days.', user));
-            }
-            msg.send(weekMoods.map(function(mood) {
-                return bars[validMoods.indexOf(mood.mood)];
-            }).join(''));
+        moodGraph.call(client, { user: user, since: 7 }, function(err, graph) {
+            if (err) return msg.send(err);
+            msg.send(graph);
         });
     });
 
@@ -167,14 +208,12 @@ module.exports = function(robot) {
         if (user.toLowerCase().trim() === "me") {
             user = msg.message.user && msg.message.user.name;
         }
-        getMoods.call(client, function(err, moods) {
-            var userMood = moods.filter(function(mood) {
-                return mood.date === today() && mood.user === user;
-            })[0];
-            if (!userMood) {
+        getMoods.call(client, {date: today(), user: user}, function(err, moods) {
+            if (err) return msg.send(err);
+            if (!moods || !moods[0]) {
                 return msg.send(format('%s has not set a mood, yet', user));
             }
-            msg.send(format('%s is in a %s mood', userMood.user, userMood.mood));
+            msg.send(format('%s: %s is in a %s mood', moods[0].date, moods[0].user, moods[0].mood));
         });
     });
 };
